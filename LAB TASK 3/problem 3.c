35f82b1a8652e07afab5f41117e63188fe503c5e
fuwad7 <hasan16-669@s.diu.edu.bd>
2022-10-02 20:52:23 +0600

    LAB TASK 3

--- LAB TASK 3/problem 3.c
+++ LAB TASK 3/problem 3.c
@@ -1,63 +1,13 @@
-//Write a C program to input week number and print week day
-
 #include<stdio.h>
+int main(){
+        int week;
+        printf("Enter the week number: ");
+        scanf("%d",&week);
 
-int main()
-{
-
-    int week;
-
-    printf("Enter Week Number: ");
-    scanf("%d",&week);
-
-    if (week==1)
-
-    {
-        printf("Saturday");
-    }
-
-    else if (week==2)
-
-    {
-        printf("Sunday");
-    }
-
-    else if (week==3)
-
-    {
-        printf("Monday");
-    }
-
-    else if (week==4)
-
-    {
-        printf("Tuesday");
-    }
-
-    else if (week==5)
-
-    {
-        printf("Wednesday");
-    }
-
-    else if (week==6)
-
-    {
-        printf("Thursday");
-    }
-
-    else if (week==7)
-
-    {
-        printf("Friday");
-
-    }
-
-    else
-
-    {
-        printf("Wrong Input");
-    }
-
-    return 0;
-}
+        if(week>=1 && week<=52){
+        printf("Saturday\nSunday\nMonday\nTuesday\nWednesday\nThrusday\nFriday\n");
+        }else{
+        printf("Invalid\n");
+        }
+        return 0;
+        }
