0d1c684de8cfd2084ed215a1ffbf314bb25f68ea
fuwad7 <hasan16-669@s.diu.edu.bd>
2022-12-16 16:12:05 +0600

    Overall Conditional Practice

--- Overall conditional practice/problem2.c
+++ Overall conditional practice/problem2.c
@@ -1,1 +1,50 @@
+//Input the value of X & Y coordinates and show which quadrant the input is as the output
 
+#include<stdio.h>
+
+int main(){
+
+    float x,y;
+    printf("Enter the value of X: ");
+    scanf("%f",&x);
+    printf("Enter the value of Y: ");
+    scanf("%f",&y);
+
+    if (x>0 && y>0)
+
+    {
+        printf("The Coordinates of (%.2f,%.2f) lies in 1st Quadrant",x,y);
+    }
+
+    else if (x<0 && y>0)
+
+    {
+        printf("The Coordinates of (%.2f,%.2f) lies in 2nd Quadrant",x,y);
+    }
+
+    else if (x<0 && y<0)
+
+    {
+        printf("The Coordinates of (%.2f,%.2f) lies in 3rd Quadrant",x,y);
+    }
+
+    else if (x>0 && y<0)
+
+    {
+        printf("The Coordinates of (%.2f,%.2f) lies in 4th Quadrant",x,y);
+    }
+
+    else if (x == 0 && y == 0)
+
+    {
+        printf("The Coordinates of (%.2f,%.2f) lies in Center",x,y);
+    }
+
+    else
+
+    {
+        printf("Invalid",x,y);
+    }
+
+    return 0;
+}
